f5facf879c3d8c4b985553720b8b35408dea6076
Adam Eastwood <adam-3@live.co.uk>
2023-03-04 01:24:10 +0000

    Initial Commit

---
+++ BikePSS/BikePSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikePSSApp.swift
+//  BikePSS
+//
+//  Created by Adam Eastwood on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BikePSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
